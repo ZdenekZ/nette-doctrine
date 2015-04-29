6706155a30e662fb0d9015879e623f3db96cf807
ZdenekZ <zdenek.zezula@seznam.cz>
2015-04-29 15:32:49 +0200

    cleaning

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -15,40 +15,17 @@ use Nette,
 class HomepagePresenter extends BasePresenter
 {
 
-
-	/** @var Nette\Database\Context @inject */
-	public $dbContext;
-
 	/** @var  EntityManager @inject */
 	public $em;
 
 	public function renderDefault()
 	{
-		$posts = $this->dbContext->table('posts')
-//			->where('id > ?', 3)
-		;
-		$postsJson = [];
-		foreach ($posts as $post) {
-//			\Tracy\Debugger::dump($post->created_at);
-			$postsJson[] = [
-				'id' => $post->id,
-				'content' => $post->content,
-				'created_at' => $post->created_at,
-			];
-		}
-//		die;
-//		$this->sendJson($postsJson);
-		$this->template->posts = $posts;
-	}
-
-	public function renderArticles()
-	{
 		$articleRepository = $this->em->getRepository(Article::class);
 		$articles = $articleRepository->createQueryBuilder('a')
 			->leftJoin('a.blog', 'b')
 			->addSelect('b')
-			->where('b.id = ?1')
-			->setParameter(1, 2)
+//			->where('b.id = ?1')
+//			->setParameter(1, 2)
 			->getQuery()->getResult()
 		;
 //		$articles = $articleRepository->findAll();
@@ -66,7 +43,7 @@ class HomepagePresenter extends BasePresenter
 		$article = new Article($blog, $name, 'asdkfhaksdhkhrtk');
 		$this->em->persist($article);
 		$this->em->flush();
-		$this->redirect('articles');
+		$this->redirect('default');
 	}
 
 
@@ -78,7 +55,7 @@ class HomepagePresenter extends BasePresenter
 		$blog = new Blog($name);
 		$this->em->persist($blog);
 		$this->em->flush();
-		$this->redirect('articles');
+		$this->redirect('default');
 	}
 
 }
